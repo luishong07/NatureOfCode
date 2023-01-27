a66923192d966303ada72670707e63fab47855a9
Luis Hong <luishong50@gmail.com>
2023-01-26 21:30:26 -0600

    created standing sine wave;

--- Chapter3/3.5-SimpleHarmonicMotion/harmonicMotion.js
+++ Chapter3/3.5-SimpleHarmonicMotion/harmonicMotion.js
@@ -1,27 +1,37 @@
-let angle = 0
-let angleV = 0
+let angles = []
+let r = 5
+let angleV = []
+
+
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  
+  let total = floor(innerWidth/(r*2))
+  for(let i = 0; i < total; i++){
+    angles[i] = map(i, 0, total, 0, TWO_PI)
+    angleV[i] = 0.01 + i/100
+  }
+
 }
 
 function draw() {
   background(0)
   translate(innerWidth/2, innerHeight/2)
-  fill(234,42,245)
-
-  //growing and shrinking circle
-  // let r = map(sin(angle), -1, 1, 0, 200)
-  // circle(0,0,r*2)
-
-
-  //moving dot up and down in sin motion
-  let y = map(sin(angle), -1, 1, -200,200)
-  circle(0,y,25)
-
-  // let increment = TWO_PI/60
-  angle += angleV
-  angleV += 0.001
+  // fill(234,42,245)
+  noFill()
+  stroke(253,245,34)
+  // print(mouseX, mouseY)
+  beginShape()
+  for(let i = 0; i < angles.length; i++){
+    let y = map(sin(angles[i]), -1, 1, -innerHeight/2, innerHeight/2)
+    strokeWeight(3)
+    let x = map(i, 0, angles.length, -innerWidth/2, innerWidth/2)
+    // line(x,0,x,y)
+    circle(x,y,r*2)
+    // vertex(x, y)
+    angles[i] += 0.02
+    // angles[i] += angleV[i]
+  }
+  endShape()
 
 
 
