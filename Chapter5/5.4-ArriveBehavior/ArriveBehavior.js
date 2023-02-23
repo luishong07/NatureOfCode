67c294d2def37495e36d0d3f55317059b5987dbb
Luis Hong <luishong50@gmail.com>
2023-02-22 21:49:38 -0600

    started with arriving behavior

---
+++ Chapter5/5.4-ArriveBehavior/ArriveBehavior.js
@@ -0,0 +1,20 @@
+let vehicle;
+
+function setup() {
+  createCanvas(400, 400);
+  vehicle = new Vehicle(100, 100);
+}
+
+function draw() {
+  background(0);
+
+  let target = createVector(mouseX, mouseY);
+  fill(255, 0, 0);
+  noStroke();
+  ellipse(target.x, target.y, 32);
+
+  let steering = vehicle.arrive(target);
+  vehicle.applyForce(steering);
+  vehicle.update();
+  vehicle.show();
+}
